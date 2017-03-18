7a4a958eb63f319f0382257f2182f862d5d4f6b1
Eldiyar Talantbek <eldiyar.talantbek@gmail.com>
2017-03-18 14:44:50 -0500

    using absolute path

--- admin.js
+++ admin.js
@@ -1,57 +1,65 @@
 var express = require('express');
 var router = express.Router();
+var rooms = require('./data/rooms.json');
 var uuid = require('node-uuid');
 var _ = require('lodash');
 
-router.get('/admin/rooms', function (req, res) {
+router.get('/rooms', function (req, res) {
     res.render('rooms',
         {
             title: "Admin rooms",
-            rooms: rooms
+            rooms: rooms,
+            request: req
         });
 });
 
-router.get('/admin/rooms/add', function (req, res) {
-    res.render('add', { title : "Create Chat Room" });
+router.get('/rooms/add', function (req, res) {
+    res.render('add',
+        {
+            title: "Create Chat Room",
+            request: req
+        });
 });
 
-router.post('/admin/rooms/add', function (req, res) {
+router.post('/rooms/add', function (req, res) {
     var room = {
-        name : req.body.name,
-        id : uuid.v4()
+        name: req.body.name,
+        id: uuid.v4()
     }
     rooms.push(room);
-    res.redirect('/admin/rooms');
+    res.redirect(req.baseUrl + "/rooms");  // /admin/rooms
 });
 
-router.get('/admin/rooms/edit/:id', function (req, res) {
+router.get('/rooms/edit/:id', function (req, res) {
     var roomId = req.params.id;
     var room = _.find(rooms, r => r.id === roomId);
-    if(!room) {
+    if (!room) {
         res.sendStatus(404);
         return;
     }
-    res.render('edit', {
-        title : 'Edit room name',
-        room : room
-    });
+    res.render('edit',
+        {
+            title: 'Edit room name',
+            room: room,
+            request: req
+        });
 });
 
-router.post('/admin/rooms/edit/:id', function (req, res) {
+router.post('/rooms/edit/:id', function (req, res) {
     var roomId = req.params.id;
     var room = _.find(rooms, r => r.id == roomId);
-    if(!room) {
+    if (!room) {
         res.sendStatus(404);
         return;
     }
     room.name = req.body.name;
-    res.redirect('/admin/rooms');
+    res.redirect(req.baseUrl + "/rooms");
 });
 
-router.get('/admin/rooms/delete/:id', function (req, res) {
+router.get('/rooms/delete/:id', function (req, res) {
     var roomId = req.params.id;
     rooms = rooms.filter(r => r.id !== roomId);
-    res.redirect('/admin/rooms');
+    res.redirect(req.baseUrl + "/rooms");
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var rooms = require('./data/rooms.json');
 var bodyParser = require('body-parser');
 var adminRouter = require('./admin');
 
@@ -9,7 +8,7 @@ var adminRouter = require('./admin');
 app.use(express.static('public')); // makes possible serving static assets
 app.use(express.static('node_modules/bootstrap/dist'));
 app.use(bodyParser.urlencoded({ extended : true }));
-app.use(adminRouter);
+app.use("/admin", adminRouter);
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
