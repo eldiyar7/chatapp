63278eef0b2d289c7f7101ea96dd175e1e2250e4
Eldiyar Talantbek <eldiyar.talantbek@gmail.com>
2017-03-20 10:12:47 -0500

    refactoring routes

--- admin.js
+++ admin.js
@@ -13,53 +13,54 @@ router.get('/rooms', function (req, res) {
         });
 });
 
-router.get('/rooms/add', function (req, res) {
-    res.render('add',
-        {
-            title: "Create Chat Room",
-            request: req
-        });
-});
+router.route('/rooms/add')
+    .get(function (req, res) {
+        res.render('add',
+            {
+                title: "Create Chat Room",
+                request: req
+            });
+    })
+    .post(function (req, res) {
+        var room = {
+            name: req.body.name,
+            id: uuid.v4()
+        }
+        rooms.push(room);
+        res.redirect(req.baseUrl + "/rooms");  // /admin/rooms
+    });
 
-router.post('/rooms/add', function (req, res) {
-    var room = {
-        name: req.body.name,
-        id: uuid.v4()
-    }
-    rooms.push(room);
-    res.redirect(req.baseUrl + "/rooms");  // /admin/rooms
-});
+router.route('/rooms/edit/:id')
+    .get(function (req, res) {
+        var roomId = req.params.id;
+        var room = _.find(rooms, r => r.id === roomId);
+        if (!room) {
+            res.sendStatus(404);
+            return;
+        }
+        res.render('edit',
+            {
+                title: 'Edit room name',
+                room: room,
+                request: req
+            });
+    })
+    .post(function (req, res) {
+        var roomId = req.params.id;
+        var room = _.find(rooms, r => r.id == roomId);
+        if (!room) {
+            res.sendStatus(404);
+            return;
+        }
+        room.name = req.body.name;
+        res.redirect(req.baseUrl + "/rooms");
+    });
 
-router.get('/rooms/edit/:id', function (req, res) {
-    var roomId = req.params.id;
-    var room = _.find(rooms, r => r.id === roomId);
-    if (!room) {
-        res.sendStatus(404);
-        return;
-    }
-    res.render('edit',
-        {
-            title: 'Edit room name',
-            room: room,
-            request: req
-        });
-});
-
-router.post('/rooms/edit/:id', function (req, res) {
-    var roomId = req.params.id;
-    var room = _.find(rooms, r => r.id == roomId);
-    if (!room) {
-        res.sendStatus(404);
-        return;
-    }
-    room.name = req.body.name;
-    res.redirect(req.baseUrl + "/rooms");
-});
-
-router.get('/rooms/delete/:id', function (req, res) {
-    var roomId = req.params.id;
-    rooms = rooms.filter(r => r.id !== roomId);
-    res.redirect(req.baseUrl + "/rooms");
-});
+router.route('/rooms/delete/:id')
+    .get(function (req, res) {
+        var roomId = req.params.id;
+        rooms = rooms.filter(r => r.id !== roomId);
+        res.redirect(req.baseUrl + "/rooms");
+    });
 
 module.exports = router;
