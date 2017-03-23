65680b851118ce399a1d10954b58460b2ecf5e8d
Eldiyar Talantbek <eldiyar.talantbek@gmail.com>
2017-03-23 11:36:32 -0500

    posting message to api

--- api.js
+++ api.js
@@ -3,26 +3,40 @@ var router = express.Router();
 var rooms = require('./data/rooms.json');
 var messages = require('./data/messages.json');
 var _ = require('lodash');
+var uuid = require('node-uuid');
 
 router.get('/rooms', function (req, res) {
     res.json(rooms);
 });
 
-router.get('/rooms/:roomId/messages', function (req, res) {
-    var roomId = req.params.roomId;
-    var roomMessages = messages.filter(m => m.roomId == roomId);
+router.route('/rooms/:roomId/messages')
+    .get(function (req, res) {
+        var roomId = req.params.roomId;
+        var roomMessages = messages.filter(m => m.roomId == roomId);
 
-    var room = _.find(rooms, r => r.id === roomId);
-    if (!room) {
-        nres.sendStatus(404);
-        return;
-    }
+        var room = _.find(rooms, r => r.id === roomId);
+        if (!room) {
+            res.sendStatus(404);
+            return;
+        }
 
-    res.json({
-        room: room,
-        messages: roomMessages
+        res.json({
+            room: room,
+            messages: roomMessages
+        });
+    })
+    .post(function (req, res) {
+        var roomId = req.params.roomId;
+
+        var message = {
+            text: req.body.text,
+            roomId: roomId,
+            userId: "44f885e8-87e9-4911-973c-4074188f408a",
+            id: uuid.v4()
+        }
+
+        messages.push(message);
+        res.sendStatus(200);
     });
-});
-
 
 module.exports = router;
