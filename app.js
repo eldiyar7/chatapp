2e9a347b782d8ffa12048d1e1279f9a01a0ca847
Eldiyar Talantbek <eldiyar.talantbek@gmail.com>
2017-03-24 12:31:00 -0500

    added dev debug tools

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+app.use(require('morgan')('combined'));
+
 app.use(express.static('public'));
 app.use(express.static('node_modules/bootstrap/dist'));
 app.use(express.static('node_modules/jquery/dist'));
 app.use(express.static('node_modules/font-awesome'));
-app.use(bodyParser.urlencoded({ extended : true })); // to parse form data
+app.use(bodyParser.urlencoded({extended: true})); // to parse form data
 app.use(bodyParser.json()); // to parse json
+
+require('express-debug')(app, {});
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.render('home', { title: "Home" });
+    res.render('home', {title: "Home"});
 });
 
 var adminRouter = require('./admin');
@@ -23,7 +27,7 @@ var apiRouter = require('./api');
 app.use('/api', apiRouter);
 
 app.get('/admin/users', function (req, res) {
-    res.render('users', { title: "Admin users" });
+    res.render('users', {title: "Admin users"});
 });
 
 app.listen(3000, function () {
